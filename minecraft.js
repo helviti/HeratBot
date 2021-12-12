dc5952cca542836d552ee0f3098c37be54c0ba18
helviti <canfurkankalaycioglu@gmail.com>
2021-12-12 15:00:25 +0300

    removed useless files

--- minecraft.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-try {
-  const data = fs.readFileSync('/home/helviti/.local/share/multimc/instances/1.15.2/.minecraft/options.txt', 'utf8');
-  console.log(data);
-} catch (e) {
-  console.log('Error:', e.stack);
-}
